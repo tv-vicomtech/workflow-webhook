ce27598d092c758103eb3c7c747b32fe2307e37b
hrivas23 <hrivas@vicomtech.org>
2021-04-09 18:14:59 +0200

    Removed webhook secret

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,18 +7,12 @@ if [ -z "$webhook_url" ]; then
     exit 1
 fi
 
-if [ -z "$webhook_secret" ]; then
-    echo "No webhook_secret configured"
-    exit 1
-fi
-
 if [ -n "$data" ]; then
     CUSTOM_JSON_DATA=$(echo -n "$data" | jq -c '')
     JSON_WITH_OPEN_CLOSE_BRACKETS_STRIPPED=`echo "$WEBHOOK_DATA" | sed 's/^{\(.*\)}$/\1/'`
     WEBHOOK_DATA="{$JSON_WITH_OPEN_CLOSE_BRACKETS_STRIPPED,\"data\":$CUSTOM_JSON_DATA}"
 fi
 
-WEBHOOK_SIGNATURE=$(echo -n "$WEBHOOK_DATA" | openssl sha1 -hmac "$webhook_secret" -binary | xxd -p)
 WEBHOOK_ENDPOINT=$webhook_url
 
 if [ -n "$webhook_auth" ]; then
@@ -29,7 +23,6 @@ fi
 curl -X POST \
     -H "Content-Type: $CONTENT_TYPE" \
     -H "User-Agent: User-Agent: GitHub-Hookshot/760256b" \
-    -H "X-Hub-Signature: sha1=$WEBHOOK_SIGNATURE" \
     -H "X-GitHub-Delivery: $GITHUB_RUN_NUMBER" \
     -H "X-GitHub-Event: $GITHUB_EVENT_NAME" \
     --data "$WEBHOOK_DATA" $WEBHOOK_ENDPOINT
